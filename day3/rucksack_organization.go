4f8eba1dd99d1f79418813579a41af2ce1a7b7ae
Mathew Sharff <mrsharff@gmail.com>
2022-12-04 23:23:43 -0800

    Day 3 Part 3

--- day3/rucksack_organization.go
+++ day3/rucksack_organization.go
@@ -12,7 +12,7 @@ import (
 // TODO: if our set is less than 64 items, we can use a uint64 to represent it
 
 type Item = rune
-
+type Rucksack = string
 type Set map[Item]struct{}
 
 func main() {
@@ -26,11 +26,62 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
 
 	b, _ := os.ReadFile("day3/input.txt")
 	part1(bytes.NewReader(b))
+
+	part2(strings.NewReader(testInput))
+	part2(bytes.NewReader(b))
+}
+
+func part2(r io.Reader) {
+	rucksacks := make(chan Rucksack)
+	go func() {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			rucksacks <- scanner.Text()
+		}
+		close(rucksacks)
+	}()
+
+	groups := make(chan [3]Rucksack)
+	go func() {
+		group := [3]Rucksack{}
+		i := 0
+		for rucksack := range rucksacks {
+			group[i] = rucksack
+			i++
+			if i == 3 {
+				groups <- group
+				i = 0
+				group = [3]Rucksack{}
+			}
+		}
+		close(groups)
+	}()
+
+	priorities := make(chan int)
+	go func() {
+		for group := range groups {
+			itemsCarriedByAll3 := intersection(newSet(group[0]), intersection(newSet(group[1]), newSet(group[2])))
+			if len(itemsCarriedByAll3) != 1 {
+				panic("the badge is the only item type carried by all three Elves")
+			}
+			for k, _ := range itemsCarriedByAll3 {
+				priorities <- priority(k)
+			}
+		}
+		close(priorities)
+	}()
+
+	total := 0
+	for p := range priorities {
+		total += p
+	}
+
+	fmt.Println(total)
+
 }
 
 func part1(r io.Reader) {
 	scanner := bufio.NewScanner(r)
-
 	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
