ae949cc06475cdf514beed8a3b568d436cf03bdd
Mathew Sharff <mrsharff@gmail.com>
2022-12-05 00:09:49 -0800

    Day 3 goroutines

--- day3/rucksack_organization.go
+++ day3/rucksack_organization.go
@@ -32,44 +32,87 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
 }
 
 func part2(r io.Reader) {
-	rucksacks := make(chan Rucksack)
-	go func() {
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			rucksacks <- scanner.Text()
-		}
-		close(rucksacks)
-	}()
+	solve(r, byGroup)
+}
 
-	groups := make(chan [3]Rucksack)
-	go func() {
-		group := [3]Rucksack{}
-		i := 0
-		for rucksack := range rucksacks {
-			group[i] = rucksack
-			i++
-			if i == 3 {
-				groups <- group
-				i = 0
-				group = [3]Rucksack{}
-			}
-		}
-		close(groups)
-	}()
+func part1(r io.Reader) {
+	solve(r, byRucksack)
+}
 
-	priorities := make(chan int)
-	go func() {
-		for group := range groups {
-			itemsCarriedByAll3 := intersection(newSet(group[0]), intersection(newSet(group[1]), newSet(group[2])))
-			if len(itemsCarriedByAll3) != 1 {
-				panic("the badge is the only item type carried by all three Elves")
-			}
-			for k, _ := range itemsCarriedByAll3 {
-				priorities <- priority(k)
-			}
+func byRucksack(rucksacks chan Rucksack, priorities chan int) {
+	for rucksack := range rucksacks {
+		var leftSet, rightSet Set
+		n := len(rucksack)
+		leftSet = newSet(rucksack[:n/2])
+		rightSet = newSet(rucksack[n/2:])
+		itemsInBothCompartments := intersection(leftSet, rightSet)
+		if len(itemsInBothCompartments) != 1 {
+			panic("The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.")
 		}
-		close(priorities)
-	}()
+		for k, _ := range itemsInBothCompartments {
+			priorities <- priority(k)
+		}
+	}
+	close(priorities)
+}
+
+func byGroup(rucksacks chan Rucksack, priorities chan int) {
+	groups := make(chan [3]Rucksack, 100)
+	go readGroups(rucksacks, groups)
+
+	for group := range groups {
+		itemsCarriedByAll3 := intersection(newSet(group[0]), intersection(newSet(group[1]), newSet(group[2])))
+		if len(itemsCarriedByAll3) != 1 {
+			panic("the badge is the only item type carried by all three Elves")
+		}
+		for k, _ := range itemsCarriedByAll3 {
+			priorities <- priority(k)
+		}
+	}
+	close(priorities)
+}
+
+func readLines(r io.Reader, lines chan string) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines <- scanner.Text()
+	}
+	close(lines)
+}
+
+func readRucksacks(lines chan string, rucksacks chan Rucksack) {
+	for line := range lines {
+		rucksacks <- line
+	}
+	close(rucksacks)
+}
+
+func readGroups(rucksacks chan Rucksack, groups chan [3]Rucksack) {
+	group := [3]Rucksack{}
+	i := 0
+	for rucksack := range rucksacks {
+		group[i] = rucksack
+		i++
+		if i == 3 {
+			groups <- group
+			i = 0
+			group = [3]Rucksack{}
+		}
+	}
+	close(groups)
+}
+
+type Prioritizer func(rucksacks chan Rucksack, priorities chan int)
+
+func solve(r io.Reader, prioritize Prioritizer) {
+	lines := make(chan string, 300)
+	go readLines(r, lines)
+
+	rucksacks := make(chan Rucksack, 300)
+	go readRucksacks(lines, rucksacks)
+
+	priorities := make(chan int, 300)
+	go prioritize(rucksacks, priorities)
 
 	total := 0
 	for p := range priorities {
@@ -77,41 +120,14 @@ func part2(r io.Reader) {
 	}
 
 	fmt.Println(total)
-
-}
-
-func part1(r io.Reader) {
-	scanner := bufio.NewScanner(r)
-	total := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		var leftSet, rightSet Set
-		n := len(line)
-		leftSet = newSet(line[:n/2])
-		rightSet = newSet(line[n/2:])
-		total += func() int {
-			var ps []int
-			for k, _ := range intersection(leftSet, rightSet) {
-				ps = append(ps, priority(k))
-			}
-			if len(ps) != 1 {
-				panic("The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.")
-			}
-			return ps[0]
-		}()
-	}
-
-	fmt.Println(total)
 }
 
 func priority(i Item) int {
-	var p int
 	if 'A' <= i && i <= 'Z' {
-		p = int(i-'A') + 27
-	} else {
-		p = int(i-'a') + 1
+		return int(i-'A') + 27
 	}
-	return p
+	return int(i-'a') + 1
+
 }
 
 func newSet(runes string) Set {
