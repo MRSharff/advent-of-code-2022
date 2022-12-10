574f354def56859ca21161d24ddc949fada7aa37
Mathew Sharff <mrsharff@gmail.com>
2022-12-10 13:21:23 -0800

    Reformat some code

--- day3/rucksack_organization.go
+++ day3/rucksack_organization.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/MRSharff/advent-of-code-2022/metrics"
 	"io"
 	"os"
 	"strings"
+
+	"github.com/MRSharff/advent-of-code-2022/metrics"
 )
 
 var (
@@ -30,11 +31,8 @@ func main() {
 	part1(bytes.NewReader(b))
 
 	part2(strings.NewReader(testInput))
-	metrics.Timeit(func() {part2(bytes.NewReader(b))})
-	metrics.Timeit(func() {solveSerial(bytes.NewReader(b), serialByRucksack)})
-	metrics.Timeit(func() {solveSerial(bytes.NewReader(b), serialByGroup)})
+	metrics.Timeit(func() { part2(bytes.NewReader(b)) })
 }
-
 
 func part2(r io.Reader) {
 	solve(r, byGroup)
@@ -42,79 +40,6 @@ func part2(r io.Reader) {
 
 func part1(r io.Reader) {
 	solve(r, byRucksack)
-}
-
-type SerialPrioritizer func(rucksacks []Rucksack) []int
-
-func solveSerial(r io.Reader, prioritize SerialPrioritizer) {
-	var lines []string
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	var rucksacks []Rucksack
-	for _, line := range lines {
-		rucksacks = append(rucksacks, line)
-	}
-
-	priorities := prioritize(rucksacks)
-
-	total := 0
-	for _, p := range priorities {
-		total += p
-	}
-	fmt.Println(total)
-}
-
-func serialByRucksack(rucksacks []Rucksack) []int {
-	var priorities []int
-	for _, rucksack := range rucksacks {
-		var leftSet, rightSet Set
-		n := len(rucksack)
-		leftSet = newSet(rucksack[:n/2])
-		rightSet = newSet(rucksack[n/2:])
-		itemsInBothCompartments := intersection(leftSet, rightSet)
-		if len(itemsInBothCompartments) != 1 {
-			panic("The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.")
-		}
-		for k, _ := range itemsInBothCompartments {
-			priorities = append(priorities, priority(k))
-		}
-	}
-	return priorities
-}
-
-func serialByGroup(rucksacks []Rucksack) []int {
-	groups := serialReadGroups(rucksacks)
-
-	var priorities []int
-	for _, group := range groups {
-		itemsCarriedByAll3 := intersection(newSet(group[0]), intersection(newSet(group[1]), newSet(group[2])))
-		if len(itemsCarriedByAll3) != 1 {
-			panic("the badge is the only item type carried by all three Elves")
-		}
-		for k, _ := range itemsCarriedByAll3 {
-			priorities = append(priorities, priority(k))
-		}
-	}
-	return priorities
-}
-
-func serialReadGroups(rucksacks []Rucksack) [][3]Rucksack {
-	var groups [][3]Rucksack
-	group := [3]Rucksack{}
-	i := 0
-	for _, rucksack := range rucksacks {
-		group[i] = rucksack
-		i++
-		if i == 3 {
-			groups = append(groups, group)
-			i = 0
-			group = [3]Rucksack{}
-		}
-	}
-	return groups
 }
 
 type Prioritizer func(rucksacks chan Rucksack, priorities chan int)
@@ -132,22 +57,6 @@ func solve(r io.Reader, prioritize Prioritizer) {
 	}
 
 	fmt.Println(total)
-}
-
-func prioritize(rucksack Rucksack) int {
-	var leftSet, rightSet Set
-	n := len(rucksack)
-	leftSet = newSet(rucksack[:n/2])
-	rightSet = newSet(rucksack[n/2:])
-	itemsInBothCompartments := intersection(leftSet, rightSet)
-	if len(itemsInBothCompartments) != 1 {
-		panic("The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.")
-	}
-	var p int
-	for k, _ := range itemsInBothCompartments {
-		p = priority(k)
-	}
-	return p
 }
 
 func byRucksack(rucksacks chan Rucksack, priorities chan int) {
