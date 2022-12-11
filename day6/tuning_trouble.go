ed2c3a007c86318996b4c1dcb2db77cab5c2b15b
Mathew Sharff <mrsharff@gmail.com>
2022-12-11 15:33:11 -0800

    Day 6 Part 2

--- day6/tuning_trouble.go
+++ day6/tuning_trouble.go
@@ -13,24 +13,35 @@ var (
 func main() {
 	part1(testInput)
 	part1(string(b))
+
+	part2(testInput)
+	part2(string(b))
 }
 
 func part1(buffer string) {
+	solve(buffer, 4)
+}
+
+func part2(buffer string) {
+	solve(buffer, 14)
+}
+
+func solve(buffer string, markerSize int) {
 	window := make(map[rune]int)
 
-	hold := make(chan rune, 4)
+	hold := make(chan rune, markerSize)
 
 	for i, c := range buffer {
-		if i == 4 {
+		if i == markerSize {
 			break
 		}
 		hold <- c
 		window[c] = window[c] + 1
 	}
 
-	i := 4
+	i := markerSize
 	for _, entering := range buffer[i:] {
-		if len(window) == 4 {
+		if len(window) == markerSize {
 			break
 		}
 		leaving := <-hold
